7a30fa8056198b800bd1821170495eda5a8fcd3a
Pilner <raileyvictuelles@gmail.com>
2024-07-15 20:31:00 +0800

    feature: implemented express app

---
+++ app.js
@@ -0,0 +1,54 @@
+// SETUP
+import * as dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const	app 	= express(),
+		port	= process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// ROUTES
+app.get("/", (req, res) => {
+	try {
+		res.render("home");
+	} catch(err) {
+		res.render("errorpage");
+		console.log(err);
+	}
+});
+
+app.get("/about", (req, res) => {
+	try {
+		res.render("about");
+	} catch
+	(err) {
+		res.render("errorpage");
+		console.log(err);
+	}
+});
+
+app.get("/form", (req, res) => {
+	try {
+		res.render("form");
+	} catch(err) {
+		res.render("errorpage");
+		console.log(err);
+	}
+});
+
+app.get("*", (req, res) => {
+	res.render("errorpage");
+});
+
+
+
+
+// LISTEN
+app.listen(port, () => {
+	console.log(`Server starting on port ${port}`);
+})
