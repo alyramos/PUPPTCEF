e0015bbb97c153da55a44c088c70c299414494e3
Pilner <raileyvictuelles@gmail.com>
2024-07-15 21:50:41 +0800

    feat(database): Added Initialization + Add API Route - Added Function and NPM command to Setup Database - Add Route on `/api/form`

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // SETUP
 import * as dotenv from "dotenv";
 import express from "express";
+import pool, { initializeDatabase } from "./db.mjs";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const	app 	= express(),
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
 
 // ROUTES
 app.get("/", (req, res) => {
@@ -41,11 +43,87 @@ app.get("/form", (req, res) => {
 	}
 });
 
+// API Submit Form Route
+app.post("/api/form", async (req, res) => {
+	try {
+		const {
+			student_name,
+			student_number,
+			student_gender,
+			student_program,
+			target_semester,
+			signature,
+			program_cp,
+			program_cp_title,
+			program_dean,
+			subject_code,
+			subject_description,
+			subject_day,
+			subject_time,
+			subject_units,
+			reason,
+			current_units,
+			cross_enroll_units,
+			total_units
+		} = req.body;
+
+		const client = await pool.connect();
+		const student = await client.query(`
+			INSERT INTO students (
+				student_name,
+				student_number,
+				student_gender,
+				student_program,
+				target_semester,
+				signature
+			) VALUES ($1, $2, $3, $4, $5, $6)
+		`, [student_name, student_number, student_gender, student_program, target_semester, signature]);
+
+		const program = await client.query(`
+			INSERT INTO programs (
+				student_id,
+				program_cp,
+				program_cp_title,
+				program_dean
+			) VALUES ($1, $2, $3, $4)
+		`, [student.rows[0].student_id, program_cp, program_cp_title, program_dean]);
+
+		const subject = await client.query(`
+			INSERT INTO subjects (
+				student_id,
+				subject_code,
+				subject_description,
+				subject_day,
+				subject_time,
+				subject_units
+			) VALUES ($1, $2, $3, $4, $5, $6)
+		`, [student.rows[0].student_id, subject_code, subject_description, subject_day, subject_time, subject_units]);
+
+		const reasons = await client.query(`
+			INSERT INTO reasons (
+				student_id,
+				reason
+			) VALUES ($1, $2)
+		`, [student.rows[0].student_id, reason]);
+
+		const crossenrollments = await client.query(`
+			INSERT INTO crossenrollments (
+				student_id,
+				current_units,
+				cross_enroll_units,
+				total_units
+			) VALUES ($1, $2, $3, $4)
+		`, [student.rows[0].student_id, current_units, cross_enroll_units, total_units]);
+
+	} catch (error) {
+		console.error(error);
+	}
+})
+
+
 app.get("*", (req, res) => {
 	res.render("errorpage");
 });
-
-
 
 
 // LISTEN
---
+++ db.mjs
@@ -0,0 +1,119 @@
+import pg from "pg";
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // REMOVE THIS IN PRODUCTION
+  }
+});
+
+export default pool;
+
+async function initializeDatabase() {
+	// Connect to Database
+	const client = await pool.connect();
+	
+	// Drop Database on start
+	client.query("DROP SCHEMA public CASCADE", (err, res) => {
+		if(err) {
+			console.log(err);
+		} else {
+			client.query("CREATE SCHEMA public", (err, res) => {
+				if(err) {
+					console.log(err);
+				} else {
+					console.log("Schema created");
+				}
+			});
+			console.log("All Tables dropped");
+		}
+	});
+
+	// Create Table
+	client.query(`
+		CREATE TABLE students (
+			student_id SERIAL PRIMARY KEY,
+			student_name VARCHAR(255),
+			student_number VARCHAR(255),
+			student_gender VARCHAR(255),
+			student_program VARCHAR(255),
+			target_semester VARCHAR(255),
+			createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			signature VARCHAR(255)
+		`, (err, res) => {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("Students Table created");
+		}
+	});
+
+	// Create Programs Table with student_id as foreign key
+	client.query(`
+		CREATE TABLE programs (
+			program_id SERIAL PRIMARY KEY,
+			student_id INT,
+			program_cp VARCHAR(255),
+			program_cp_title VARCHAR(255),
+			program_dean VARCHAR(255),
+			FOREIGN KEY (student_id) REFERENCES students(student_id)
+		`, (err, res) => {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("Programs Table created");
+		}
+	});
+
+	// Create Subjects Table with student_id as foreign key
+	client.query(`
+		CREATE TABLE subjects (
+			subject_id SERIAL PRIMARY KEY,
+			student_id INT,
+			subject_code VARCHAR(255),
+			subject_description VARCHAR(255),
+			subject_day VARCHAR(255),
+			subject_time VARCHAR(255),
+			subject_units INT,
+			FOREIGN KEY (student_id) REFERENCES students(student_id)
+		`, (err, res) => {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("Subjects Table created");
+		}
+	});
+
+	// Create Reasons Table with student_id as foreign key
+	client.query(`
+		CREATE TABLE reasons (
+			reason_id SERIAL PRIMARY KEY,
+			student_id INT,
+			reason VARCHAR(255),
+			FOREIGN KEY (student_id) REFERENCES students(student_id)
+		`, (err, res) => {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("Reasons Table created");
+		}
+	});
+
+	// Create CrossEnrollments Table with student_id as foreign key
+	client.query(`
+		CREATE TABLE crossenrollments (
+			cross_enrollment_id SERIAL PRIMARY KEY,
+			student_id INT,
+			current_units INT,
+			cross_enroll_units INT,
+			total_units INT,
+			FOREIGN KEY (student_id) REFERENCES students(student_id)
+		`, (err, res) => {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("CrossEnrollments Table created");
+		}
+	});
+}
